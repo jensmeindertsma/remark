75e9d154277332e43ddb9919f6f6b6912eb5c806
Jens Meindertsma <mail@jensmeindertsma.com>
2023-10-28 19:30:59 +0200

    Fix old problems

--- app/root.tsx
+++ app/root.tsx
@@ -1,28 +1,27 @@
 import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-  } from "@remix-run/react";
-  
-  export default function App() {
-    return (
-      <html>
-        <head>
-          <link
-            rel="icon"
-            href="data:image/x-icon;base64,AA"
-          />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <h1>Hello world!!!!</h1>
-          <Outlet />
-  
-          <Scripts />
-        </body>
-      </html>
-    );
-  }
-  
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
--- scripts/build-server.ts
+++ scripts/build-server.ts
@@ -3,6 +3,10 @@ import esbuild from "esbuild";
 await esbuild.build({
   entryPoints: ["./server/main.ts"],
   platform: "node",
+  format: "esm",
   bundle: true,
   outfile: "./build/server/main.js",
+  loader: {
+    ".node": "file",
+  },
 });
